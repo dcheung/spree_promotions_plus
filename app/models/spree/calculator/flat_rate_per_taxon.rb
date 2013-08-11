53ff13d097a7d5ea4ca8a9086abb4ff92c5144ce
Alvaro Sanchez <alvaro.sanchez@the-cocktail.com>
2013-08-11 19:32:41 +0200

    Fix bug => En la promo "Descuento en cantidad según categoría" la cantidad descontada por producto no tiene que superar el precio de dicho producto!

--- app/models/spree/calculator/flat_rate_per_taxon.rb
+++ app/models/spree/calculator/flat_rate_per_taxon.rb
@@ -12,7 +12,7 @@ module Spree
 		def compute(object=nil)
 			return 0 if object.nil?
 			taxon = Spree::Taxon.find(preferred_taxon)
-			value = object.line_items.map{|li| li.product.first_taxon.ancestors.include?(taxon) ? li.quantity*self.preferred_amount : 0.0 }.sum
+			value = object.line_items.map{|li| li.product.first_taxon.ancestors.include?(taxon) ? (li.price > self.preferred_amount ? (li.quantity * self.preferred_amount) : 0.0) : 0.0 }.sum
 			(value * 100).round.to_f / 100
 		end
 
