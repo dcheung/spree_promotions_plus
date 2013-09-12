b0113abfcb55774f2facb6f59d8496987bedca8f
Alvaro Sanchez <alvaro.sanchez@the-cocktail.com>
2013-09-12 18:23:56 +0200

    cambios routes para json request

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Spree::Core::Engine.routes.draw do
   # Add your extension routes here
+  namespace :admin do
+  	match '/taxons/load_selection' => 'taxons#load_selection', :format => :json
+  end
 end
